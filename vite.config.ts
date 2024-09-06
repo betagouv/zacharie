024d7ceaaa20d7931c11fd1008d8f795463bccb5
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-09-06 18:11:00 +0200

    vite config instead

--- vite.config.ts
+++ vite.config.ts
@@ -28,4 +28,10 @@ export default defineConfig({
   build: {
     sourcemap: true,
   },
+  // https://github.com/prisma/prisma/issues/12504#issuecomment-1285883083
+  resolve: {
+    alias: {
+      ".prisma/client/index-browser": "./node_modules/.prisma/client/index-browser.js",
+    },
+  },
 });
