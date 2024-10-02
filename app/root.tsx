21ee35f44ee28b14674d998f2bc7afe3a4c615b9
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-10-02 17:19:38 +0200

    manifest is back

--- app/root.tsx
+++ app/root.tsx
@@ -77,7 +77,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {/* <link rel="manifest" href={dsfrWebManifest} crossOrigin="use-credentials" /> */}
         <link rel="stylesheet" href={dsfrCss} />
         <link rel="stylesheet" href={dsfrColorCss} />
-        {import.meta.env.SPA_MODE && <PWAManifest />}
+        {process.env.SPA_MODE && <PWAManifest />}
         <Meta />
         <Links />
       </head>
