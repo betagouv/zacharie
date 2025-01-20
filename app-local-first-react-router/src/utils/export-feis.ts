44ffbb9f30c98d9d6365b12c362d9c09e483ee26
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-01-20 12:19:13 +0100

    fix export feis

--- app-local-first-react-router/src/utils/export-feis.ts
+++ app-local-first-react-router/src/utils/export-feis.ts
@@ -1,16 +1,57 @@
 import { useState } from 'react';
-import { utils, write, writeFile } from '@e965/xlsx';
+import { utils, writeFile } from '@e965/xlsx';
 import useZustandStore from '@app/zustand/store';
 import type { UserForFei } from '~/src/types/user';
 import { EntityWithUserRelation } from '@api/src/types/entity';
 import { getUserRoleLabel } from './get-user-roles-label';
 import dayjs from 'dayjs';
-import JSZip from 'jszip';
 import { getCarcasseIntermediaireId } from './get-carcasse-intermediaire-id';
 import { loadFei } from './load-fei';
 import { capture } from '@app/services/sentry';
 
-function createSheet(data: Array<Record<string, unknown>>) {
+type FeiExcelData = {
+  Donnée: string;
+  Valeur: string | null;
+};
+
+type CarcasseExcelData = {
+  'Numéro de bracelet': string;
+  Éspèce: string | null;
+  "Nombre d'animaux": number | null | undefined;
+  'Numéro suivi trichine': string | null;
+  Estampille: string | null;
+  // infos de SVI
+  'SVI - Consigne': string | null;
+  'SVI - Motif Consigne': string | null;
+  'SVI - Commentaire': string | null;
+  'SVI - Saisie partielle': string | null;
+  'SVI - Saisie totale': string | null;
+  'SVI - Saisie motif': string;
+  'SVI - Certificat de saisie OK': string | null;
+  "SVI - Date d'examen": Date | null;
+  // infos de chasse
+  'Premier détenteur': string;
+  'Date de la chasse': string;
+  'Commune de la chasse': string | null;
+  'Numéro de fiche': string;
+  // Observations ETG
+  'Commentaires ETG / Transporteurs': string | null;
+  // Plus d'infos
+  'Heure de première mise à mort': string | null;
+  'Heure de dernière éviscération': string | null;
+  // zacharie_carcasse_id: string;
+  // 'Type de gibier': string;
+  // 'Examen initial - Anomalies carcasse': string;
+  // 'Examen initial - Anomalies abats': string;
+  // 'Examen initial - Commentaire': string | null;
+  // Manquante: string;
+  // 'Refusée par un destinataire': string;
+  // Commentaires: string;
+};
+
+function createSheet<T extends keyof CarcasseExcelData | keyof FeiExcelData>(
+  data: Array<Record<T, unknown>>,
+) {
   /*
   [
     [the, first, array, is, the, header],
@@ -19,7 +60,8 @@ function createSheet(data: Array<Record<string, unknown>>) {
    */
 
   const header = [
-    ...data.reduce((columns: Array<string>, item: Record<string, unknown>) => {
+    //   const wscols = header.map((col: keyof CarcasseExcelData | keyof FeiExcelData) => {
+    ...data.reduce((columns: Array<T>, item: Record<T, unknown>) => {
       for (let key of Object.keys(item)) {
         if (!columns.find((col) => col === key)) columns.push(key);
       }
@@ -75,27 +117,35 @@ function createSheet(data: Array<Record<string, unknown>>) {
   }
 
   // Set column widths
+  // col is key of CarcasseExcelData
   const wscols = header.map((col) => {
     switch (col) {
-      case 'Commentaires':
       case 'Donnée':
       case 'Valeur':
+      case 'Commentaires ETG / Transporteurs':
       case 'SVI - Commentaire':
-      case 'Examen initial - Anomalies carcasse':
-      case 'Examen initial - Anomalies abats':
-      case 'Examen initial - Commentaire':
-      case 'SVI - Saisie':
       case 'SVI - Saisie motif':
+      case 'Premier détenteur':
+      case 'Commune de la chasse':
+      case 'Numéro de fiche':
         return { wch: 40 }; // wider columns for comments
-      case 'zacharie_carcasse_id':
-        return { wch: 30 }; // wider columns for comments
       // case 'Numéro de bracelet':
-      case 'Éspèce':
-      case 'Type de gibier':
+      case 'SVI - Saisie totale':
+      case 'SVI - Certificat de saisie OK':
+      case 'Estampille':
       case "Nombre d'animaux":
-      case 'Manquante':
+      case 'SVI - Consigne':
+      case 'Date de la chasse':
+      case 'Heure de première mise à mort':
+      case 'Heure de dernière éviscération':
         return { wch: 15 };
       // case 'Refusée par un destinataire':
+      case 'Numéro suivi trichine':
+      case 'Numéro de bracelet':
+      case 'Éspèce':
+      case 'SVI - Saisie partielle':
+      case 'SVI - Motif Consigne':
+      case "SVI - Date d'examen":
       default:
         return { wch: 20 }; // default width
     }
@@ -145,33 +195,9 @@ export default function useExportFeis() {
     // just to trigger the loading state, sorry Raph :)
     await new Promise((res) => setTimeout(res));
 
-    const zip = new JSZip();
-
     const carcassesWorkbook = utils.book_new();
-    const allCarcasses:
-      | Record<string, unknown>[]
-      | {
-          'Numéro de fiche': string;
-          'Date de la chasse': string;
-          'Heure de première mise à mort': string | null;
-          'Heure de dernière éviscération': string | null;
-          Commune: string | null;
-          // zacharie_carcasse_id: string;
-          'Numéro de bracelet': string;
-          Éspèce: string | null;
-          // 'Type de gibier': string;
-          "Nombre d'animaux": number | null;
-          'Examen initial - Anomalies carcasse': string;
-          'Examen initial - Anomalies abats': string;
-          'Examen initial - Commentaire': string | null;
-          Manquante: string;
-          'Refusée par un destinataire': string;
-          Commentaires: string;
-          'SVI - Saisie': string;
-          'SVI - Saisie motif': string;
-          'SVI - Commentaire': string | null;
-          'SVI - Date de signature': Date | null;
-        }[] = [];
+    const feiSheets: Record<string, Array<FeiExcelData>> = {};
+    const allCarcasses: Array<CarcasseExcelData> = [];
 
     try {
       for (const feiId of feiNumbers) {
@@ -184,7 +210,6 @@ export default function useExportFeis() {
             continue;
           }
         }
-        const workbook = utils.book_new();
 
         const feiSheetData = [
           {
@@ -203,7 +228,10 @@ export default function useExportFeis() {
         ];
         const examinateurInitial = users[fei.examinateur_initial_user_id!];
         if (examinateurInitial) {
-          feiSheetData.push({ Donnée: 'Examinateur initial', Valeur: formatUser(examinateurInitial) });
+          feiSheetData.push({
+            Donnée: 'Examinateur initial',
+            Valeur: formatUser(examinateurInitial),
+          });
         }
         const premierDetenteur = users[fei.premier_detenteur_user_id!];
         const premierDetenteurEntity = entities[fei.premier_detenteur_entity_id!];
@@ -213,7 +241,10 @@ export default function useExportFeis() {
             Valeur: `${formatEntity(premierDetenteurEntity)}\n${formatUser(premierDetenteur)}`,
           });
         } else if (premierDetenteur) {
-          feiSheetData.push({ Donnée: 'Premier détenteur', Valeur: formatUser(premierDetenteur) });
+          feiSheetData.push({
+            Donnée: 'Premier détenteur',
+            Valeur: formatUser(premierDetenteur),
+          });
         }
 
         const intermediaires = state.getFeiIntermediairesForFeiNumero(fei.numero);
@@ -227,7 +258,10 @@ export default function useExportFeis() {
             });
           }
         } else {
-          feiSheetData.push({ Donnée: 'Destinataire', Valeur: 'Pas encore réceptionné' });
+          feiSheetData.push({
+            Donnée: 'Destinataire',
+            Valeur: 'Pas encore réceptionné',
+          });
         }
 
         const sviEntity = entities[fei.svi_entity_id!];
@@ -236,95 +270,95 @@ export default function useExportFeis() {
           Valeur: formatEntity(sviEntity) || 'Pas encore désigné',
         });
 
-        utils.book_append_sheet(workbook, createSheet(feiSheetData), 'Données de chasse');
-        utils.book_append_sheet(carcassesWorkbook, createSheet(feiSheetData), fei.numero);
+        feiSheets[fei.numero] = feiSheetData;
 
-        const carcasses = (state.carcassesIdsByFei[fei.numero] || [])
-          .map((carcasseId) => {
-            const carcasse = state.carcasses[carcasseId];
-            if (carcasse.deleted_at) return null;
-            const commentaires = [];
-            for (const intermediaire of intermediaires) {
-              if (intermediaire.deleted_at) continue;
-              const carcassesIntermediairesId = getCarcasseIntermediaireId(
-                fei.numero,
-                carcasse.numero_bracelet,
-                intermediaire.id,
+        for (const carcasseId of state.carcassesIdsByFei[fei.numero] || []) {
+          const carcasse = state.carcasses[carcasseId];
+          if (!carcasse) {
+            console.error('carcasse not found', carcasseId);
+            continue;
+          }
+          if (carcasse.deleted_at) {
+            console.error('carcasse deleted', carcasseId);
+            continue;
+          }
+          if (carcasse.intermediaire_carcasse_manquante) {
+            console.error('carcasse manquante', carcasseId);
+            continue;
+          }
+          if (carcasse.intermediaire_carcasse_refus_motif) {
+            console.error('carcasse refusée', carcasseId);
+            continue;
+          }
+          const commentaires = [];
+          for (const intermediaire of intermediaires) {
+            if (intermediaire.deleted_at) continue;
+            const carcassesIntermediairesId = getCarcasseIntermediaireId(
+              fei.numero,
+              carcasse.numero_bracelet,
+              intermediaire.id,
+            );
+            const intermediaireCarcasse = state.carcassesIntermediaires[carcassesIntermediairesId];
+            if (intermediaireCarcasse?.commentaire) {
+              const intermediaireEntity = state.entities[intermediaire.fei_intermediaire_entity_id];
+              commentaires.push(
+                `${intermediaireEntity?.nom_d_usage} : ${intermediaireCarcasse?.commentaire}`,
               );
-              const intermediaireCarcasse = state.carcassesIntermediaires[carcassesIntermediairesId];
-              if (intermediaireCarcasse?.commentaire) {
-                const intermediaireEntity = state.entities[intermediaire.fei_intermediaire_entity_id];
-                commentaires.push(
-                  `${intermediaireEntity?.nom_d_usage} : ${intermediaireCarcasse?.commentaire}`,
-                );
-              }
             }
-            const toReturn = {
-              // zacharie_carcasse_id: carcasse.zacharie_carcasse_id,
-              'Numéro de bracelet': carcasse.numero_bracelet,
-              Éspèce: carcasse.espece,
-              // 'Type de gibier': carcasse.type === CarcasseType.PETIT_GIBIER ? 'Petit gibier' : 'Gros gibier',
-              "Nombre d'animaux": carcasse.nombre_d_animaux,
-              'Premier détenteur':
-                premierDetenteurEntity?.nom_d_usage || premierDetenteur?.nom_de_famille
-                  ? `${premierDetenteur?.prenom} ${premierDetenteur?.nom_de_famille}`
-                  : '',
-              Manquante: carcasse.intermediaire_carcasse_manquante ? 'Oui' : '',
-              'Refusée par un destinataire': carcasse.intermediaire_carcasse_refus_motif || '',
-              'SVI - Saisie': carcasse.svi_carcasse_saisie.join(' - '),
-              'SVI - Saisie motif': carcasse.svi_carcasse_saisie_motif.join('\n'),
-              'SVI - Commentaire': carcasse.svi_carcasse_commentaire,
-              'SVI - Date de signature': carcasse.svi_carcasse_signed_at,
-              'Examen initial - Anomalies carcasse': carcasse.examinateur_anomalies_carcasse.join(', '),
-              'Examen initial - Anomalies abats': carcasse.examinateur_anomalies_abats.join(', '),
-              'Examen initial - Commentaire': carcasse.examinateur_commentaire,
-              Commentaires: commentaires.join('\n'),
-            };
-            allCarcasses.push({
-              ...toReturn,
-              'Numéro de fiche': fei.numero,
-              'Date de la chasse': dayjs(fei.date_mise_a_mort).format('DD/MM/YYYY'),
-              'Heure de première mise à mort': fei.heure_mise_a_mort_premiere_carcasse,
-              'Heure de dernière éviscération': fei.heure_evisceration_derniere_carcasse,
-              Commune: fei.commune_mise_a_mort,
-            });
-            return toReturn;
-          })
-          .filter((carcasse) => carcasse !== null);
-
-        utils.book_append_sheet(workbook, createSheet(carcasses), 'Carcasses');
-        const fileName = `Fiche ${fei.numero} - ${dayjs(fei.updated_at).format('YYYY-MM-DD-HH-mm')}.xlsx`;
-
-        if (feiNumbers.length > 1) {
-          const buffer = write(workbook, { type: 'buffer', cellStyles: true, bookSST: true });
-          zip.file(fileName, buffer);
-        } else {
-          writeFile(workbook, fileName, { cellStyles: true, bookSST: true });
-          return setIsExporting(false);
+          }
+          allCarcasses.push({
+            'Numéro de bracelet': carcasse.numero_bracelet,
+            Éspèce: carcasse.espece,
+            "Nombre d'animaux": carcasse.nombre_d_animaux || 1,
+            'Numéro suivi trichine': '',
+            Estampille: '',
+            // infos de SVI
+            'SVI - Consigne': '',
+            'SVI - Motif Consigne': 'BA P S OA CA pap',
+            'SVI - Commentaire': carcasse.svi_carcasse_commentaire,
+            'SVI - Saisie partielle':
+              carcasse.svi_carcasse_saisie?.[0] === 'Saisie partielle'
+                ? carcasse.svi_carcasse_saisie.filter((_s, i) => i > 0).join(' - ')
+                : '',
+            'SVI - Saisie totale': carcasse.svi_carcasse_saisie?.[0] === 'Saisie totale' ? 'Oui' : '',
+            'SVI - Saisie motif': carcasse.svi_carcasse_saisie_motif.join('\n'),
+            'SVI - Certificat de saisie OK': '',
+            "SVI - Date d'examen": carcasse.svi_carcasse_signed_at,
+            // infos de chasse
+            'Premier détenteur':
+              premierDetenteurEntity?.nom_d_usage ||
+              (premierDetenteur?.nom_de_famille
+                ? `${premierDetenteur?.prenom} ${premierDetenteur?.nom_de_famille}`
+                : ''),
+            'Date de la chasse': dayjs(fei.date_mise_a_mort).format('DD/MM/YYYY'),
+            'Commune de la chasse': fei.commune_mise_a_mort,
+            'Numéro de fiche': fei.numero,
+            // Observations ETG
+            'Commentaires ETG / Transporteurs': commentaires.join('\n'),
+            // Plus d'infos
+            'Heure de première mise à mort': fei.heure_mise_a_mort_premiere_carcasse,
+            'Heure de dernière éviscération': fei.heure_evisceration_derniere_carcasse,
+            // 'Examen initial - Anomalies carcasse': carcasse.examinateur_anomalies_carcasse.join(', '),
+            // 'Examen initial - Anomalies abats': carcasse.examinateur_anomalies_abats.join(', '),
+            // 'Examen initial - Commentaire': carcasse.examinateur_commentaire,
+            // Manquante: '';
+            // 'Refusée par un destinataire': '';
+            // Commentaires: '';
+          });
         }
       }
 
-      utils.book_append_sheet(carcassesWorkbook, createSheet(allCarcasses), 'Carcasses');
-      // Move the sheet to the first position
-      const wb = carcassesWorkbook;
-      const lastSheet = wb.SheetNames[wb.SheetNames.length - 1];
-      wb.SheetNames.pop();
-      wb.SheetNames.unshift(lastSheet);
+      utils.book_append_sheet(carcassesWorkbook, createSheet(allCarcasses), 'Carcasses', true);
 
-      zip.file('Carcasses.xlsx', write(carcassesWorkbook, { type: 'buffer' }));
+      for (const [feiNumero, feiSheetData] of Object.entries(feiSheets)) {
+        utils.book_append_sheet(carcassesWorkbook, createSheet(feiSheetData), feiNumero, true);
+      }
 
-      // Generate zip blob
-      const zipContent = await zip.generateAsync({ type: 'blob' });
-
-      // Create download link and trigger download
-      const downloadUrl = URL.createObjectURL(zipContent);
-      const link = document.createElement('a');
-      link.href = downloadUrl;
-      link.download = `exports-zacharie-${dayjs().format('YYYY-MM-DD-HH-mm')}.zip`;
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-      URL.revokeObjectURL(downloadUrl);
+      writeFile(carcassesWorkbook, `export-carcasses-zacharie-${dayjs().format('YYYY-MM-DD-HH-mm')}.xlsx`, {
+        cellStyles: true,
+        bookSST: true,
+      });
+      return setIsExporting(false);
 
       // actions
     } catch (e: unknown) {
