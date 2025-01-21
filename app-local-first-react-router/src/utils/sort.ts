ba33e1d51e9d70da8d1ac4f1c4b93ac523131cb8
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2025-01-21 13:59:55 +0100

    feat tri des carcasses réceptionnées par etg pour impression pour svi (#26)

--- app-local-first-react-router/src/utils/sort.ts
+++ app-local-first-react-router/src/utils/sort.ts
@@ -1,3 +1,5 @@
+import { Carcasse } from '@prisma/client';
+
 export function sortLatestFirstByCreatedAt<T extends { created_at: string }>(a: T, b: T) {
   return new Date(b.created_at).getTime() - new Date(a.created_at).getTime();
 }
@@ -12,4 +14,36 @@ export function sortEarliestFirstByCreatedAt<T extends { created_at: string }>(a
 
 export function sortEarliestFirstByUpdatedAt<T extends { updated_at: string }>(a: T, b: T) {
   return new Date(a.updated_at).getTime() - new Date(b.updated_at).getTime();
+}
+
+export function sortCarcassesApproved(carcasseA: Carcasse, carcasseB: Carcasse) {
+  if (carcasseA.intermediaire_carcasse_signed_at && carcasseB.intermediaire_carcasse_signed_at) {
+    return (
+      carcasseA.intermediaire_carcasse_signed_at.getTime() -
+      carcasseB.intermediaire_carcasse_signed_at.getTime()
+    );
+  }
+  if (carcasseA.espece === carcasseB.espece) {
+    return carcasseA.numero_bracelet.localeCompare(carcasseB.numero_bracelet);
+  }
+  if (carcasseA.type === carcasseB.type) {
+    return carcasseA.espece!.localeCompare(carcasseB.espece!);
+  }
+  return carcasseA.type!.localeCompare(carcasseB.type!);
+}
+
+export function sortCarcassesApprovedForExcel(carcasseA: Carcasse, carcasseB: Carcasse) {
+  if (carcasseA.intermediaire_carcasse_signed_at && carcasseB.intermediaire_carcasse_signed_at) {
+    return (
+      carcasseA.intermediaire_carcasse_signed_at.getTime() -
+      carcasseB.intermediaire_carcasse_signed_at.getTime()
+    );
+  }
+  if (carcasseA.espece === carcasseB.espece) {
+    return carcasseA.numero_bracelet.localeCompare(carcasseB.numero_bracelet);
+  }
+  if (carcasseA.type === carcasseB.type) {
+    return carcasseA.espece!.localeCompare(carcasseB.espece!);
+  }
+  return carcasseA.type!.localeCompare(carcasseB.type!);
 }
