14a7cbaf673bbce863d4980adfe364606ae509f1
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-04-10 15:50:49 +0200

    fix bug quand un examinateur peut envoyer une fiche a un premier détenteur mais quil nen est pas un lui même

--- api-express/src/controllers/fei.ts
+++ api-express/src/controllers/fei.ts
@@ -11,25 +11,25 @@ import { feiDoneSelect, feiPopulatedInclude } from '~/types/fei';
 import { formatCountCarcasseByEspece } from '~/utils/count-carcasses';
 // import { refreshMaterializedViews } from '~/utils/refreshMaterializedViews';
 
-prisma.fei
-  .findMany({
-    where: {
-      // numero: 'ZACH-20250114-EVHMN-154237',
-    },
-    include: {
-      Carcasses: true,
-    },
-  })
-  .then(async (feis) => {
-    for (const fei of feis) {
-      const nombreDAnimaux = formatCountCarcasseByEspece(fei.Carcasses).filter(Boolean).join('\n');
-      await prisma.fei.update({
-        where: { numero: fei.numero },
-        data: { resume_nombre_de_carcasses: nombreDAnimaux },
-      });
-    }
-    console.log('done');
-  });
+// prisma.fei
+//   .findMany({
+//     where: {
+//       // numero: 'ZACH-20250114-EVHMN-154237',
+//     },
+//     include: {
+//       Carcasses: true,
+//     },
+//   })
+//   .then(async (feis) => {
+//     for (const fei of feis) {
+//       const nombreDAnimaux = formatCountCarcasseByEspece(fei.Carcasses).filter(Boolean).join('\n');
+//       await prisma.fei.update({
+//         where: { numero: fei.numero },
+//         data: { resume_nombre_de_carcasses: nombreDAnimaux },
+//       });
+//     }
+//     console.log('done');
+//   });
 
 router.post(
   '/:fei_numero',
@@ -820,19 +820,23 @@ router.get(
               {
                 examinateur_initial_user_id: user.id,
               },
-              {
-                premier_detenteur_user_id: user.id,
-              },
-              {
-                FeiPremierDetenteurEntity: {
-                  EntityRelationsWithUsers: {
-                    some: {
-                      owner_id: user.id,
-                      relation: EntityRelationType.WORKING_FOR,
+              ...(user.roles.includes(UserRoles.PREMIER_DETENTEUR)
+                ? [
+                    {
+                      premier_detenteur_user_id: user.id,
                     },
-                  },
-                },
-              },
+                    {
+                      FeiPremierDetenteurEntity: {
+                        EntityRelationsWithUsers: {
+                          some: {
+                            owner_id: user.id,
+                            relation: EntityRelationType.WORKING_FOR,
+                          },
+                        },
+                      },
+                    },
+                  ]
+                : []),
               {
                 FeiIntermediaires: {
                   some: {
