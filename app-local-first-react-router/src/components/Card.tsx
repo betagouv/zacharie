eb410d2aa4ee6760eb83fd9b2531554f4da0f738
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-04-24 16:36:38 +0200

    nouvelle icone carcasses

--- app-local-first-react-router/src/components/Card.tsx
+++ app-local-first-react-router/src/components/Card.tsx
@@ -122,9 +122,9 @@ export default function Card({ fei, onPrintSelect, isPrintSelected = false }: Ca
           'flex size-full shrink-0 flex-col gap-3 bg-none p-6 !no-underline hover:!bg-active-tint hover:!no-underline',
 
           carcassesRefusées.length > 0
-            ? 'border-error-main-525 border-l-2'
+            ? 'border-l-2 border-error-main-525'
             : simpleStatus === 'Clôturée'
-              ? 'border-success-main-525 border-l-2'
+              ? 'border-l-2 border-success-main-525'
               : '',
         ].join(' ')}
       >
@@ -176,7 +176,7 @@ export default function Card({ fei, onPrintSelect, isPrintSelected = false }: Ca
                   <div>
                     {carcassesRefusées.map((line) => {
                       return (
-                        <p className="text-error-main-525 m-0 text-xl font-semibold" key={line}>
+                        <p className="m-0 text-xl font-semibold text-error-main-525" key={line}>
                           {line}
                         </p>
                       );
@@ -222,9 +222,9 @@ function ChasseIcon() {
 
 function CarcassesIcon() {
   return (
-    <svg width="19" height="16" viewBox="0 0 19 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-        d="M13.3704 9.00701L15.1297 10.7391C9.14812 16.6283 4.22223 16.6283 0 15.2426L15.4815 0L19 3.46422L13.3704 9.00701ZM10.5556 9.00701L16.1852 3.46422L15.4815 2.77138L4.03912 14.0372C6.75754 14.1424 9.37104 13.1491 12.2657 10.6907L10.5556 9.00701Z"
+        d="M7.27273 2.96065C5.04313 3.27956 3.27956 5.04313 2.96065 7.27273H4.36364V8.72727H2.96065C3.27956 10.9569 5.04313 12.7204 7.27273 13.0393V11.6364H8.72727V13.0393C10.9569 12.7204 12.7204 10.9569 13.0393 8.72727H11.6364V7.27273H13.0393C12.7204 5.04313 10.9569 3.27956 8.72727 2.96065V4.36364H7.27273V2.96065ZM1.49449 7.27273C1.82988 4.23882 4.23882 1.82988 7.27273 1.49449V0H8.72727V1.49449C11.7612 1.82988 14.1701 4.23882 14.5055 7.27273H16V8.72727H14.5055C14.1701 11.7612 11.7612 14.1701 8.72727 14.5055V16H7.27273V14.5055C4.23882 14.1701 1.82988 11.7612 1.49449 8.72727H0V7.27273H1.49449ZM9.45455 8C9.45455 8.80335 8.80335 9.45455 8 9.45455C7.19665 9.45455 6.54545 8.80335 6.54545 8C6.54545 7.19665 7.19665 6.54545 8 6.54545C8.80335 6.54545 9.45455 7.19665 9.45455 8Z"
         fill="black"
       />
     </svg>
