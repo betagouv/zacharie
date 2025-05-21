4a2ec2de28225b530bae2493498b3f4eed5e7217
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-05-21 09:43:33 +0200

    upgrade deps

--- app-local-first-react-router/src/routes/mon-profil/mes-informations.tsx
+++ app-local-first-react-router/src/routes/mon-profil/mes-informations.tsx
@@ -314,7 +314,7 @@ export default function MesInformations() {
                         onClick: () => setAssoExpanded(true),
                       }}
                     >
-                      Enregistrer mon association
+                      Enregistrer mon entité
                     </Button>
                   </>
                 ) : (
