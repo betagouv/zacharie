6cdddbfd9b7966d1e8cc831d0229a5c9196a4c52
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-05-23 11:06:12 +0200

    rajouter le nom de l etg dans le amil pour le svi

--- api-express/src/controllers/fei.ts
+++ api-express/src/controllers/fei.ts
@@ -360,6 +360,7 @@ router.post(
           body: `Vous avez une nouvelle fiche à traiter. Rendez-vous sur Zacharie pour la traiter.`,
           email: [
             `Carcasses à inspecter :\n${savedFei.resume_nombre_de_carcasses}`,
+            `ETG concerné: ${savedFei.FeiCurrentEntity.nom_d_usage}`,
             `Rendez-vous sur Zacharie pour consulter le détail de la fiche\u00A0:\nhttps://zacharie.beta.gouv.fr/app/tableau-de-bord/fei/${savedFei.numero}`,
           ].join('\n'),
           notificationLogAction: `FEI_ASSIGNED_TO_${savedFei.fei_next_owner_role}_${savedFei.numero}`,
