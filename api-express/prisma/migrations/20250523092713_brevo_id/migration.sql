1533390a28e33457fcf990516e074b9c8986b6fa
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-05-23 11:45:43 +0200

    brevo_id

---
+++ api-express/prisma/migrations/20250523092713_brevo_id/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "ETGAndEntityRelations" ADD COLUMN     "brevo_id" INTEGER;
+
+-- AlterTable
+ALTER TABLE "Entity" ADD COLUMN     "brevo_id" INTEGER;
+
+-- AlterTable
+ALTER TABLE "EntityAndUserRelations" ADD COLUMN     "brevo_id" INTEGER;
