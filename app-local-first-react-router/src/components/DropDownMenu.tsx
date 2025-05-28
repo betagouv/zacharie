83b5bb9a39822c1e6bda0bf91e2f21521015816b
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-05-28 14:16:10 +0200

    fix css

--- app-local-first-react-router/src/components/DropDownMenu.tsx
+++ app-local-first-react-router/src/components/DropDownMenu.tsx
@@ -11,7 +11,7 @@ export default function DropDownMenu({ isActive, text, menuLinks, className = ''
   return (
     <MainNavigation
       // prettier-ignore
-      className={["[&_.fr-nav\\_\\_item]:!bg-white [&_.fr-nav\\_\\_btn]:!py-0 [&_.fr-nav\\_\\_btn]:!min-h-10 [&_.fr-nav\\_\\_btn]:!text-action-high-blue-france [&_.fr-nav\\_\\_btn]:!font-medium [&_.fr-nav\\_\\_btn]:!text-base hidden lg:block", className].join(' ')}
+      className={["drop-down", "hidden lg:block", className].join(' ')}
       items={[
         {
           text,
