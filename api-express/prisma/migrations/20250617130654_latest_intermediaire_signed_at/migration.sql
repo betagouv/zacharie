91ac959c15458511ddd479aad96dec4e94c46a1e
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-06-17 15:49:44 +0200

    remove intermediaire_carcasse_signed_at

--- api-express/prisma/migrations/20250617130654_latest_intermediaire_signed_at/migration.sql
+++ api-express/prisma/migrations/20250617130654_latest_intermediaire_signed_at/migration.sql
@@ -1,6 +1,2 @@
--- Add the new column
-ALTER TABLE "Carcasse" ADD COLUMN "latest_intermediaire_signed_at" TIMESTAMP(3);
-
--- Copy data from old column to new column
-UPDATE "Carcasse" 
-SET "latest_intermediaire_signed_at" = "intermediaire_carcasse_signed_at";
+-- AlterTable
+ALTER TABLE "Carcasse" ADD COLUMN     "latest_intermediaire_signed_at" TIMESTAMP(3);
---
+++ api-express/prisma/migrations/20250617134105_remove_intermediaire_carcasse_signed_at/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `intermediaire_carcasse_signed_at` on the `Carcasse` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Carcasse" DROP COLUMN "intermediaire_carcasse_signed_at";
