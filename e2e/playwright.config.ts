1a0a5f9b46f25fba419c83e7ec6edc4af348cf28
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-07-04 16:17:14 +0200

    test: tableau de bord sans fiche

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -51,23 +51,32 @@ const config: PlaywrightTestConfig = {
 
   /* Configure projects for major browsers */
   projects: [
+    /* 
+firefox doesn't work for mobile tests
     {
       name: "firefox",
       use: {
         ...devices["Desktop Firefox"],
       },
     },
+ */
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+      },
+    },
 
-    ...(!!process.env.CI
-      ? []
-      : [
-          {
-            name: "chromium",
-            use: {
-              ...devices["Desktop Chrome"],
-            },
-          },
-        ]),
+    // ...(!!process.env.CI
+    //   ? []
+    //   : [
+    //       {
+    //         name: "chromium",
+    //         use: {
+    //           ...devices["Desktop Chrome"],
+    //         },
+    //       },
+    //     ]),
 
     // {
     //   name: "webkit",
---
+++ e2e/tests/comptes-vides-mobile.spec.ts
@@ -0,0 +1,65 @@
+import { test, expect } from "@playwright/test";
+import { resetDb } from "../scripts/reset-db";
+import { connectWith } from "../scripts/connect-with";
+
+// test.beforeAll(async () => {
+//   await resetDb();
+// });
+
+test.use({
+  viewport: { width: 350, height: 667 },
+  hasTouch: true,
+  isMobile: true,
+});
+
+test.describe("Connexion sur mobile", () => {
+  test("Connexion avec compte examinateur initial", async ({ page }) => {
+    await connectWith(page, "examinateur@example.fr");
+    await expect(page).toHaveURL("http://localhost:3290/app/tableau-de-bord");
+    await expect(page.getByRole("heading", { name: "Vous n'avez pas encore de" })).toBeVisible();
+    await expect(page.locator("#content").getByRole("button", { name: "Nouvelle fiche" }).first()).toBeVisible();
+    await expect(page.getByRole("button", { name: "Mettre à jour" }).nth(1)).not.toBeVisible();
+    await expect(page.getByRole("button", { name: "Filtrer" }).first()).toBeVisible();
+    await expect(page.getByRole("button", { name: "Action sur les fiches sélectionnées" }).first()).not.toBeVisible();
+  });
+
+  test("Connexion avec compte premier détenteur", async ({ page }) => {
+    await connectWith(page, "premier-detenteur@example.fr");
+    await expect(page).toHaveURL("http://localhost:3290/app/tableau-de-bord");
+    await expect(page.getByRole("heading", { name: "Vous n'avez pas encore de" })).toBeVisible();
+    await expect(page.locator("#content").getByRole("button", { name: "Nouvelle fiche" }).first()).not.toBeVisible();
+    await expect(page.getByRole("button", { name: "Mettre à jour" }).nth(1)).not.toBeVisible();
+    await expect(page.getByRole("button", { name: "Filtrer" }).first()).toBeVisible();
+    await expect(page.getByRole("button", { name: "Action sur les fiches sélectionnées" }).first()).not.toBeVisible();
+  });
+});
+
+test("Connexion avec compte collecteur pro", async ({ page }) => {
+  await connectWith(page, "collecteur-pro@example.fr");
+  await expect(page).toHaveURL("http://localhost:3290/app/tableau-de-bord");
+  await expect(page.getByRole("heading", { name: "Vous n'avez pas encore de" })).toBeVisible();
+  await expect(page.locator("#content").getByRole("button", { name: "Nouvelle fiche" })).not.toBeVisible();
+  await expect(page.getByRole("button", { name: "Mettre à jour" }).nth(1)).not.toBeVisible();
+  await expect(page.getByRole("button", { name: "Filtrer" }).first()).toBeVisible();
+  await expect(page.getByRole("button", { name: "Action sur les fiches sélectionnées" }).first()).not.toBeVisible();
+});
+
+test("Connexion avec compte établissement de traitement", async ({ page }) => {
+  await connectWith(page, "etg@example.fr");
+  await expect(page).toHaveURL("http://localhost:3290/app/tableau-de-bord");
+  await expect(page.getByRole("heading", { name: "Vous n'avez pas encore de" })).toBeVisible();
+  await expect(page.locator("#content").getByRole("button", { name: "Nouvelle fiche" })).not.toBeVisible();
+  await expect(page.getByRole("button", { name: "Mettre à jour" }).nth(1)).not.toBeVisible();
+  await expect(page.getByRole("button", { name: "Filtrer" }).first()).toBeVisible();
+  await expect(page.getByRole("button", { name: "Action sur les fiches sélectionnées" }).first()).not.toBeVisible();
+});
+
+test("Connexion avec compte svi", async ({ page }) => {
+  await connectWith(page, "svi@example.fr");
+  await expect(page).toHaveURL("http://localhost:3290/app/tableau-de-bord");
+  await expect(page.getByRole("heading", { name: "Vous n'avez pas encore de" })).toBeVisible();
+  await expect(page.locator("#content").getByRole("button", { name: "Nouvelle fiche" })).not.toBeVisible();
+  await expect(page.getByRole("button", { name: "Mettre à jour" }).nth(1)).not.toBeVisible();
+  await expect(page.getByRole("button", { name: "Filtrer" }).first()).toBeVisible();
+  await expect(page.getByRole("button", { name: "Action sur les fiches sélectionnées" }).first()).not.toBeVisible();
+});
