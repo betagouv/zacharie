b381874742327a4d4a54abfe740077b897327260
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-07-29 20:33:11 +0200

    logs for cookie

--- api-express/src/middlewares/passport.ts
+++ api-express/src/middlewares/passport.ts
@@ -11,10 +11,10 @@ interface JwtPayload {
 
 export default function configurePassport(app: Express) {
   const cookieExtractor = (req: Request): string | null => {
-    // console.log('Cookies extractor called', req.cookies);
+    console.log('Cookies extractor called', req.cookies);
     const token = req && req.cookies ? req.cookies['zacharie_express_jwt'] : null;
-    // console.log('Extracted token:', token);
-    // console.log('Secret key', SECRET);
+    console.log('Extracted token:', token);
+    console.log('Secret key', SECRET);
     return token;
   };
 
@@ -28,40 +28,40 @@ export default function configurePassport(app: Express) {
     new JwtStrategy(
       jwtOptions,
       async (jwt_payload: JwtPayload, done: (error: Error | null, user: User | null) => void) => {
-        // console.log('JWT Strategy called');
-        // console.log('JWT payload:', jwt_payload);
+        console.log('JWT Strategy called');
+        console.log('JWT payload:', jwt_payload);
 
         try {
           if (!jwt_payload || typeof jwt_payload !== 'object') {
-            // console.log('Invalid JWT payload');
+            console.log('Invalid JWT payload');
             return done(null, null);
           }
 
           if (!jwt_payload.userId) {
-            // console.log('No userId in JWT payload');
+            console.log('No userId in JWT payload');
             return done(null, null);
           }
 
-          // console.log('Attempting to find user with ID:', jwt_payload.userId);
+          console.log('Attempting to find user with ID:', jwt_payload.userId);
           const user = await prisma.user.findUnique({
             where: { id: jwt_payload.userId },
           });
 
           if (user) {
-            // console.log('User found:', user.email);
-            // console.log('User found:', user.id);
+            console.log('User found:', user.email);
+            console.log('User found:', user.id);
             if (user.deleted_at) {
-              // console.log('User has been deleted');
+              console.log('User has been deleted');
               return done(null, null);
             }
-            // console.log('Authentication successful');
+            console.log('Authentication successful');
             return done(null, user);
           } else {
-            // console.log('User not found in database');
+            console.log('User not found in database');
             return done(null, null);
           }
         } catch (error) {
-          // console.error('Error in Passport strategy:', error);
+          console.error('Error in Passport strategy:', error);
           return done(error as Error, null);
         }
       },
--- api-express/src/utils/cookie.ts
+++ api-express/src/utils/cookie.ts
@@ -4,6 +4,7 @@ export const JWT_MAX_AGE = 60 * 60 * 24 * 365 * 10; // 10 years in seconds
 export const COOKIE_MAX_AGE = JWT_MAX_AGE * 1000;
 
 export function cookieOptions(secureInDev = true) {
+  console.log('IS_DEV_OR_TEST', IS_DEV_OR_TEST);
   if (IS_DEV_OR_TEST) {
     return {
       maxAge: COOKIE_MAX_AGE,
@@ -12,6 +13,7 @@ export function cookieOptions(secureInDev = true) {
       sameSite: secureInDev ? ('none' as const) : ('lax' as const),
     };
   }
+  console.log('PROD COOKIE OPTIONS');
   return {
     maxAge: COOKIE_MAX_AGE,
     httpOnly: true,
@@ -22,6 +24,7 @@ export function cookieOptions(secureInDev = true) {
 }
 
 export function logoutCookieOptions(secureInDev = true) {
+  console.log('IS_DEV_OR_TEST', IS_DEV_OR_TEST);
   if (IS_DEV_OR_TEST) {
     return {
       httpOnly: true,
@@ -29,6 +32,7 @@ export function logoutCookieOptions(secureInDev = true) {
       sameSite: secureInDev ? ('none' as const) : ('lax' as const),
     };
   }
+  console.log('PROD LOGOUT COOKIE OPTIONS');
   return {
     httpOnly: true,
     secure: true,
