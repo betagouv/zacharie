5d546d0e282210bda28b2d9b21a63331b4a67ca6
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-08-01 17:28:35 +0200

    remove log relations

---
+++ api-express/prisma/migrations/20250801152804_remove_log_relations/migration.sql
@@ -0,0 +1,11 @@
+-- DropForeignKey
+ALTER TABLE "Log" DROP CONSTRAINT "Log_entity_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Log" DROP CONSTRAINT "Log_fei_numero_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Log" DROP CONSTRAINT "Log_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Log" DROP CONSTRAINT "Log_zacharie_carcasse_id_fkey";
