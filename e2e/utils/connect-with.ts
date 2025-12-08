2ab91a0150d0ab68fa0aad804a10d9f1d0953f30
Tangi Mendès <tangimds@gmail.com>
2025-12-08 14:35:56 +0100

    Update connect-with.ts

--- e2e/utils/connect-with.ts
+++ e2e/utils/connect-with.ts
@@ -3,7 +3,6 @@ import { expect, type Page } from "@playwright/test";
 export async function connectWith(page: Page, email: string, password: string = "secret-secret") {
   await page.goto("http://localhost:3290/");
   await page.getByRole("link", { name: "Se connecter" }).first().click();
-  await page.getByRole("button", { name: "Me connecter" }).waitFor({ state: "visible" });
   await page.getByRole("textbox", { name: "Mon email Renseignez votre" }).fill(email);
   await page.getByRole("textbox", { name: "Mon mot de passe Veuillez" }).fill(password);
   await page.getByRole("button", { name: "Me connecter" }).click();
