a66289735f5c30873d2fe0be5422491d00a65614
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2025-12-11 17:54:24 +0100

    fix data synced

--- e2e/tests/fiche_circuit-long_simple_examinateur.spec.ts
+++ e2e/tests/fiche_circuit-long_simple_examinateur.spec.ts
@@ -24,8 +24,8 @@ test.describe("Fiches examinateur initial", () => {
 
     await connectWith(page, "examinateur@example.fr");
     await expect(page).toHaveURL("http://localhost:3290/app/tableau-de-bord");
-    await expect(page.getByText("Synchronisation en cours")).toBeVisible();
-    await expect(page.getByText("Synchronisation en cours")).not.toBeVisible();
+    // await expect(page.getByText("Synchronisation en cours")).toBeVisible();
+    // await expect(page.getByText("Synchronisation en cours")).not.toBeVisible();
     await page.getByRole("button", { name: "Nouvelle fiche" }).nth(1).click();
     await expect(page.getByRole("heading", { name: "Examen initial Étape 1 sur" })).toBeVisible();
     await expect(page.getByText("Étape suivante : Validation")).toBeVisible();
@@ -76,13 +76,20 @@ test.describe("Fiches examinateur initial", () => {
       .getByRole("textbox", { name: "Heure d'éviscération de la" })
       .fill(dayjs().add(-1, "hour").format("HH:mm"));
     await page.getByRole("textbox", { name: "Heure d'éviscération de la" }).blur();
+    await expect(page.getByText("Synchronisation en cours")).toBeVisible();
+    await expect(page.getByText("Synchronisation en cours")).not.toBeVisible();
     await page.getByRole("button", { name: "Cliquez ici pour définir la date du jour et maintenant" }).click();
     await page.getByText("Je, Martin Marie, certifie qu").click();
-    await new Promise((resolve) => setTimeout(resolve, 100));
+    await expect(page.getByText("Synchronisation en cours")).toBeVisible();
+    await expect(page.getByText("Synchronisation en cours")).not.toBeVisible();
     await expect(page.getByRole("button", { name: "Enregistrer la fiche", exact: true })).not.toBeDisabled();
     await page.getByRole("button", { name: "Enregistrer la fiche", exact: true }).click();
+    await expect(page.getByText("Synchronisation en cours")).toBeVisible();
+    await expect(page.getByText("Synchronisation en cours")).not.toBeVisible();
     await page.getByRole("button", { name: "Pierre Petit" }).click();
     await page.getByRole("button", { name: "Valider l’examen initial" }).click();
+    await expect(page.getByText("Synchronisation en cours")).toBeVisible();
+    await expect(page.getByText("Synchronisation en cours")).not.toBeVisible();
     await expect(page.getByRole("heading", { name: "Attribution effectuée" })).toBeVisible();
     // get fei id
     const feiId = page.url().split("/").pop()!;
