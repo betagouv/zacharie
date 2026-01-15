6dfb4e5a8b9bb15c90dda2920b9373d1e0bc911d
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2026-01-15 12:21:02 +0200

    chore: disable sentry in dev

--- app-local-first-react-router/src/main.tsx
+++ app-local-first-react-router/src/main.tsx
@@ -35,49 +35,49 @@ declare module '@codegouvfr/react-dsfr/spa' {
   }
 }
 
-// if (import.meta.env.VITE_ENV === 'prod' || import.meta.env.VITE_ENV === 'test') {
-console.log('Sentry init', import.meta.env.VITE_ENV);
-Sentry.init({
-  dsn: 'https://1d9011c5042e5a03ff25dec68be1be2b@sentry.incubateur.net/199',
-  environment: `app-local-first-react-router-${import.meta.env.VITE_ENV}-2`,
-  release: __VITE_BUILD_ID__,
-  integrations: [
-    Sentry.reactRouterV6BrowserTracingIntegration({
-      useEffect: React.useEffect,
-      useLocation,
-      useNavigationType,
-      createRoutesFromChildren,
-      matchRoutes,
-    }),
-  ],
-  maxValueLength: 10000,
-  normalizeDepth: 10,
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 0.05,
-  ignoreErrors: [
-    'Network request failed',
-    // "Failed to fetch",
-    'NetworkError',
-    // ???
-    'withrealtime/messaging',
-    // This error seems to happen only in firefox and to be ignorable.
-    // The "fetch" failed because user has navigated.
-    // Since other browsers don't have this problem, we don't care about it,
-    // it may be a false positive.
-    'AbortError: The operation was aborted',
-    // Sur safari, on a des erreur de type "TypeError: cancelled" qui seraient liées
-    // au bouton "X" (ou refresh) pressé pendant un fetch. Il semblerait que la meilleure
-    // approche soit de les ignorer.
-    // Cf: https://stackoverflow.com/a/60860369/978690
-    'TypeError: cancelled',
-    'TypeError: annulé',
-  ],
-});
-// } else {
-//   console.log('Sentry not init', import.meta.env.VITE_ENV);
-// }
+if (import.meta.env.VITE_ENV === 'prod' || import.meta.env.VITE_ENV === 'test') {
+  console.log('Sentry init', import.meta.env.VITE_ENV);
+  Sentry.init({
+    dsn: 'https://1d9011c5042e5a03ff25dec68be1be2b@sentry.incubateur.net/199',
+    environment: `app-local-first-react-router-${import.meta.env.VITE_ENV}-2`,
+    release: __VITE_BUILD_ID__,
+    integrations: [
+      Sentry.reactRouterV6BrowserTracingIntegration({
+        useEffect: React.useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes,
+      }),
+    ],
+    maxValueLength: 10000,
+    normalizeDepth: 10,
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 0.05,
+    ignoreErrors: [
+      'Network request failed',
+      // "Failed to fetch",
+      'NetworkError',
+      // ???
+      'withrealtime/messaging',
+      // This error seems to happen only in firefox and to be ignorable.
+      // The "fetch" failed because user has navigated.
+      // Since other browsers don't have this problem, we don't care about it,
+      // it may be a false positive.
+      'AbortError: The operation was aborted',
+      // Sur safari, on a des erreur de type "TypeError: cancelled" qui seraient liées
+      // au bouton "X" (ou refresh) pressé pendant un fetch. Il semblerait que la meilleure
+      // approche soit de les ignorer.
+      // Cf: https://stackoverflow.com/a/60860369/978690
+      'TypeError: cancelled',
+      'TypeError: annulé',
+    ],
+  });
+} else {
+  console.log('Sentry not init', import.meta.env.VITE_ENV);
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
