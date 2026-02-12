adf72efc4ffecce2611db844eaba3e3f7ed3ddfc
Arnaud AMBROSELLI <arnaud@ambroselli.io>
2026-02-12 10:32:43 +0200

    clean merge

--- app-local-first-react-router/src/zustand/store.ts
+++ app-local-first-react-router/src/zustand/store.ts
@@ -234,42 +234,38 @@ const useZustandStore = create<State & Actions>()(
           if (newIntermediaires.length === 0) return;
           return new Promise((resolve) => {
             const feiNumero = newIntermediaires[0].fei_numero;
-            const carcasses = filterCarcassesForFei(useZustandStore.getState().carcasses, newIntermediaire.fei_numero).
-            const byId: Record<FeiAndCarcasseAndIntermediaireIds, CarcasseIntermediaire> = {};
-            const intermediairesByFei = useZustandStore.getState().intermediairesByFei;
-            const nextIntermediairesForFei = [...(intermediairesByFei[feiNumero] || [])];
+            const carcasses = filterCarcassesForFei(useZustandStore.getState().carcasses, feiNumero);
             const byId: Record<FeiAndCarcasseAndIntermediaireIds, CarcasseIntermediaire> = {};
             for (const newIntermediaire of newIntermediaires) {
-              nextIntermediairesForFei.push(newIntermediaire);
               const carcassesIntermediaires: Array<CarcasseIntermediaire> = carcasses
-              .filter((c) => !c.intermediaire_carcasse_refus_intermediaire_id);
-              .map((c) => ({
-                fei_numero: c.fei_numero,
-                numero_bracelet: c.numero_bracelet,
-                zacharie_carcasse_id: c.zacharie_carcasse_id,
-                intermediaire_id: newIntermediaire.id,
-                intermediaire_entity_id: newIntermediaire.intermediaire_entity_id,
-                intermediaire_role: newIntermediaire.intermediaire_role,
-                intermediaire_user_id: newIntermediaire.intermediaire_user_id,
-                check_manuel: null,
-                manquante: null,
-                refus: null,
-                nombre_d_animaux_acceptes: null,
-                commentaire: null,
-                decision_at: null,
-                ecarte_pour_inspection: false,
-                prise_en_charge: true, // always true by default, confirmed by the intermediaire globally
-                prise_en_charge_at: newIntermediaire.prise_en_charge_at, // will be set by the intermediaire when he confirms all the carcasse
-                intermediaire_depot_type: null,
-                intermediaire_depot_entity_id: null,
-                intermediaire_prochain_detenteur_role_cache: null,
-                intermediaire_prochain_detenteur_id_cache: null,
-                intermediaire_poids: null,
-                created_at: newIntermediaire.created_at,
-                updated_at: newIntermediaire.created_at,
-                deleted_at: null,
-                is_synced: false,
-              }));
+                .filter((c) => !c.intermediaire_carcasse_refus_intermediaire_id)
+                .map((c) => ({
+                  fei_numero: c.fei_numero,
+                  numero_bracelet: c.numero_bracelet,
+                  zacharie_carcasse_id: c.zacharie_carcasse_id,
+                  intermediaire_id: newIntermediaire.id,
+                  intermediaire_entity_id: newIntermediaire.intermediaire_entity_id,
+                  intermediaire_role: newIntermediaire.intermediaire_role,
+                  intermediaire_user_id: newIntermediaire.intermediaire_user_id,
+                  check_manuel: null,
+                  manquante: null,
+                  refus: null,
+                  nombre_d_animaux_acceptes: null,
+                  commentaire: null,
+                  decision_at: null,
+                  ecarte_pour_inspection: false,
+                  prise_en_charge: true, // always true by default, confirmed by the intermediaire globally
+                  prise_en_charge_at: newIntermediaire.prise_en_charge_at, // will be set by the intermediaire when he confirms all the carcasse
+                  intermediaire_depot_type: null,
+                  intermediaire_depot_entity_id: null,
+                  intermediaire_prochain_detenteur_role_cache: null,
+                  intermediaire_prochain_detenteur_id_cache: null,
+                  intermediaire_poids: null,
+                  created_at: newIntermediaire.created_at,
+                  updated_at: newIntermediaire.created_at,
+                  deleted_at: null,
+                  is_synced: false,
+                }));
 
               for (const ci of carcassesIntermediaires) {
                 const feiAndCarcasseAndIntermediaireId = getFeiAndCarcasseAndIntermediaireIds(ci);
