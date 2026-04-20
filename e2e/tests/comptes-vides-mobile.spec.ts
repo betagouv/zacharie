958876c912e40d956059f8a7120e4920ad6d824b
Tangi Mendès <tangimds@gmail.com>
2026-04-20 15:11:12 +0200

    up

--- e2e/tests/comptes-vides-mobile.spec.ts
+++ e2e/tests/comptes-vides-mobile.spec.ts
@@ -48,8 +48,7 @@ test("Connexion avec compte établissement de traitement", async ({ page }) => {
   await expect(page.getByRole("heading", { name: "Pas encore de fiches cette saison" })).toBeVisible();
   await expect(page.locator("#content").getByRole("button", { name: "Nouvelle fiche" })).not.toBeVisible();
   await expect(page.getByRole("button", { name: "Mettre à jour" }).nth(1)).not.toBeVisible();
-  await expect(page.getByRole("button", { name: "Filtrer" }).first()).toBeVisible();
-  await expect(page.getByRole("button", { name: "Actions" }).first()).toBeVisible();
+  await expect(page.getByRole("button", { name: "Filtres" }).first()).toBeVisible();
 });
 
 test("Connexion avec compte svi", async ({ page }) => {
