e5d6af214579642a569b71574d68f1649e279d0d
Tangi Mendès <tangimds@gmail.com>
2026-05-13 12:15:29 +0200

    chore: remove legacy code

--- app-local-first-react-router/src/routes/tableau-de-bord/tableau-de-bord-router.tsx
+++ app-local-first-react-router/src/routes/tableau-de-bord/tableau-de-bord-router.tsx
@@ -15,16 +15,6 @@ import OnboardingMesInformationsDeChasse from '@app/routes/onboarding/3b-mes-inf
 import OnboardingMonEntreprise from '@app/routes/onboarding/3-mon-entreprise';
 import OnboardingMesNotifications from '@app/routes/onboarding/4-mes-notifications';
 
-// mon profil
-import MesRoles from '@app/routes/mon-profil/1-mon-activite';
-import MesCoordonnees from '@app/routes/mon-profil/2-mes-coordonnees';
-import MesInformationsDeChasse from '@app/routes/mon-profil/3-mes-informations-de-chasse';
-import MonEntreprise from '@app/routes/mon-profil/3-mon-entreprise';
-import UtilisateursDeMonEntreprise from '@app/routes/mon-profil/3-utilisateurs-de-mon-entreprise';
-import MesNotifications from '@app/routes/mon-profil/4-mes-notifications';
-import PartageDeMesDonnees from '@app/routes/mon-profil/partage-de-mes-donnees';
-import CCGEdit from '@app/routes/mon-profil/ccg-edit';
-
 export default function RouterTableauDeBord({ navigation }: { navigation: MainNavigationProps.Item[] }) {
   return (
     <Route
@@ -98,108 +88,6 @@ export default function RouterTableauDeBord({ navigation }: { navigation: MainNa
         element={
           <RestrictedRoute id="zacharie_carcasse_id">
             <SviInspectionCarcasse />
-          </RestrictedRoute>
-        }
-      />
-      {/* Mon profil */}
-      <Route
-        path="mon-profil/mon-activite"
-        element={
-          <RestrictedRoute id="mon-activite">
-            <MesRoles />
-          </RestrictedRoute>
-        }
-      />
-      <Route
-        path="mon-profil/mes-coordonnees"
-        element={
-          <RestrictedRoute id="mes-coordonnees">
-            <MesCoordonnees />
-          </RestrictedRoute>
-        }
-      />
-      <Route
-        path="mon-profil/mes-informations-de-chasse"
-        element={
-          <RestrictedRoute id="mes-informations-de-chasse">
-            <MesInformationsDeChasse
-              withExaminateurInitial
-              withAssociationsDeChasse
-              withCCGs
-              withPartenaires
-            />
-          </RestrictedRoute>
-        }
-      />
-      <Route
-        path="mon-profil/mes-associations-de-chasse"
-        element={
-          <RestrictedRoute id="mes-associations-de-chasse">
-            <MesInformationsDeChasse withAssociationsDeChasse />
-          </RestrictedRoute>
-        }
-      />
-      <Route
-        path="mon-profil/mes-partenaires"
-        element={
-          <RestrictedRoute id="mes-partenaires">
-            <MesInformationsDeChasse withPartenaires />
-          </RestrictedRoute>
-        }
-      />
-      <Route
-        path="mon-profil/mes-ccgs"
-        element={
-          <RestrictedRoute id="mes-ccgs">
-            <MesInformationsDeChasse withCCGs />
-          </RestrictedRoute>
-        }
-      />
-      <Route
-        path="mon-profil/mes-ccgs/:entityId"
-        element={
-          <RestrictedRoute id="ccg-edit">
-            <CCGEdit />
-          </RestrictedRoute>
-        }
-      />
-      <Route
-        path="mon-profil/mon-entreprise"
-        element={
-          <RestrictedRoute id="mon-entreprise">
-            <MonEntreprise />
-          </RestrictedRoute>
-        }
-      />
-      <Route
-        path="mon-entreprise/informations"
-        element={
-          <RestrictedRoute id="mon-entreprise-informations">
-            <MonEntreprise />
-          </RestrictedRoute>
-        }
-      />
-      <Route
-        path="mon-entreprise/utilisateurs"
-        element={
-          <RestrictedRoute id="mon-entreprise-utilisateurs">
-            <UtilisateursDeMonEntreprise />
-          </RestrictedRoute>
-        }
-      />
-      <Route
-        path="mon-profil/mes-notifications"
-        element={
-          <RestrictedRoute id="mes-notifications">
-            <MesNotifications />
-          </RestrictedRoute>
-        }
-      />
-      <Route
-        path="mon-profil/partage-de-mes-donnees"
-        element={
-          <RestrictedRoute id="partage-de-mes-donnees">
-            <PartageDeMesDonnees />
           </RestrictedRoute>
         }
       />
