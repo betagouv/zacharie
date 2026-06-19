2a2fc0324ae39c42259867a5bf8aeabdd0d45326
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2026-06-19 11:56:19 +0300

    chore: fix tests (#472)

--- api-express/src/controllers/webhooks.ts
+++ api-express/src/controllers/webhooks.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import passport from 'passport';
 import { catchErrors } from '~/middlewares/errors';
 const router: express.Router = express.Router();
 import crypto from 'crypto';
@@ -9,11 +8,9 @@ router.post(
   '/sentry',
   catchErrors(async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     // verify the signature
-    console.log({ body: req.body, headers: req.headers });
     const hmac = crypto.createHmac('sha256', SENTRY_SECRET as string);
     hmac.update(JSON.stringify(req.body), 'utf8');
     const digest = hmac.digest('hex');
-    console.log({ digest, signature: req.headers['sentry-hook-signature'] });
     if (digest !== req.headers['sentry-hook-signature']) {
       // wrong signature, do nothing
       res.status(200).send({
