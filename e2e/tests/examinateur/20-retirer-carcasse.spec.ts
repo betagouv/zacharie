c1ca5e63466751821ca697840343739332a922ca
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2026-07-03 17:04:02 +0300

    fix: la transmission doit être consistante TOUT LE TEMPS (#519)
    * fix: la transmission doit être consistante TOUT LE TEMPS
    
    * fix latest_intermediaire_signed_at

--- e2e/tests/examinateur/20-retirer-carcasse.spec.ts
+++ e2e/tests/examinateur/20-retirer-carcasse.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '../../utils/test';
 import dayjs from 'dayjs';
 import 'dayjs/locale/fr';
 import utc from 'dayjs/plugin/utc';
