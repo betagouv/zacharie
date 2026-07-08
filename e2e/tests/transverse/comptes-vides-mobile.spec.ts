ce7e3a80bef7d231d941ba76752c18fa080addd7
Tangi Mendès <tangimds@gmail.com>
2026-07-08 12:42:04 +0200

    Update comptes-vides-mobile.spec.ts

--- e2e/tests/transverse/comptes-vides-mobile.spec.ts
+++ e2e/tests/transverse/comptes-vides-mobile.spec.ts
@@ -31,7 +31,6 @@ test('Connexion avec compte collecteur pro', async ({ page }) => {
   await expect(page).toHaveURL('http://localhost:3290/app/collecteur');
   await expect(page.getByRole('heading', { name: 'Pas encore de fiches cette saison' })).toBeVisible();
   await expect(page.locator('#content').getByRole('button', { name: 'Nouvelle' })).not.toBeVisible();
-  await expect(page.getByRole('button', { name: 'Exporter' }).first()).toBeVisible();
 });
 
 test('Connexion avec compte établissement de traitement', async ({ page }) => {
