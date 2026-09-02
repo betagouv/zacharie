94ecf5bddf9fc6b97613138b04f7c7ed8bb2b064
Tangi Mendès <tangimds@gmail.com>
2026-09-02 11:57:33 +0200

    Update chasseur-router.tsx

--- app-local-first-react-router/src/routes/chasseur/chasseur-router.tsx
+++ app-local-first-react-router/src/routes/chasseur/chasseur-router.tsx
@@ -17,18 +17,6 @@ import ChasseurCCGEdit from './profil/chasseur-ccg-edit';
 import ChasseurSviInspectionCarcasseLoader from './chasseur-svi-inspection-carcasse';
 import ChasseurDemandesDeModification from './chasseur-demandes-de-modification';
 import ChasseurDemandeDeModificationDetail from './chasseur-demande-de-modification-detail';
-import { TRICHINE_FEATURE_ENABLED } from '@app/utils/trichine';
-import TrichineLayout from '@app/routes/trichine/trichine-layout';
-import TrichineIndex from '@app/routes/trichine/trichine-index';
-import TrichineEchantillons from '@app/routes/trichine/trichine-echantillons';
-import TrichineEchantillonDetail from '@app/routes/trichine/trichine-echantillon-detail';
-import TrichinePools from '@app/routes/trichine/trichine-pools';
-import TrichinePoolDetail from '@app/routes/trichine/trichine-pool-detail';
-import TrichinePrelevementWizard from '@app/routes/trichine/trichine-prelevement-wizard';
-import TrichineFTPs from '@app/routes/trichine/trichine-ftps';
-import TrichineNouveauPool from '@app/routes/trichine/trichine-nouveau-pool';
-import TrichineNouvelleFTP from '@app/routes/trichine/trichine-nouvelle-ftp';
-import TrichineFTPDetail from '@app/routes/trichine/trichine-ftp-detail';
 import ChangerMonMotDePasse from '@app/routes/profil/changer-mon-mot-de-passe';
 
 export default function RouterChasseur() {
@@ -142,56 +130,6 @@ export default function RouterChasseur() {
         path="demandes-de-modification/:request_id"
         element={<ChasseurDemandeDeModificationDetail />}
       />
-      {/* Routes invisibles en production tant que le feature flag trichine n'est pas activé */}
-      {TRICHINE_FEATURE_ENABLED && (
-        <>
-          <Route
-            path="trichine"
-            element={<TrichineLayout />}
-          >
-            <Route
-              index
-              element={<TrichineIndex />}
-            />
-            <Route
-              path="echantillons"
-              element={<TrichineEchantillons />}
-            />
-            <Route
-              path="echantillons/:reference"
-              element={<TrichineEchantillonDetail />}
-            />
-            <Route
-              path="pools"
-              element={<TrichinePools />}
-            />
-            <Route
-              path="pools/:reference"
-              element={<TrichinePoolDetail />}
-            />
-            <Route
-              path="pools/:reference/2e-intention"
-              element={<TrichinePrelevementWizard />}
-            />
-            <Route
-              path="ftp"
-              element={<TrichineFTPs />}
-            />
-            <Route
-              path="ftp/:reference"
-              element={<TrichineFTPDetail />}
-            />
-            <Route
-              path="nouveau-pool"
-              element={<TrichineNouveauPool />}
-            />
-            <Route
-              path="nouvelle-ftp"
-              element={<TrichineNouvelleFTP />}
-            />
-          </Route>
-        </>
-      )}
     </Route>
   );
 }
